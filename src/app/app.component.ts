b28c5ac53037e8913ba38f59b3d3334d06591b78
Childers <childersjw@leidos.com>
2017-06-12 16:55:43 -0700

    Made some changes to index page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Chipotle Buddy';
+  links = [
+    new IndexLink("https://www.google.com/","Enter a new meal"),
+    new IndexLink("https://www.google.com/","View your progress"),
+  ];
+}
+
+export class IndexLink {
+  url : string;
+  description : string;
+  constructor(url : string, description : string) {
+    this.url = url;
+    this.description = description;
+  }
 }
